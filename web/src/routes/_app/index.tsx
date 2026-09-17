be9378d72aea1fd7649e2d87749b7a8360c47b4f
Quadgence <quadgence@gmail.com>
2026-09-17 09:46:10 +0600

    home page takes index designer

--- web/src/routes/_app/index.tsx
+++ web/src/routes/_app/index.tsx
@@ -1,11 +1,11 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { useTranslation } from 'react-i18next'
-
+import DynamicComponent from '@/components/designer/DynamicComponent'
 export const Route = createFileRoute('/_app/')({
   component: HomePage,
 })
 
 function HomePage() {
-  const { t } = useTranslation()
-  return <h1>{t('app.home', 'FormForge')}</h1>
+  return (
+    <DynamicComponent designerId='index' />
+  )
 }
